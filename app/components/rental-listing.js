9ba0234c449a2aa28038baacf6de7f26d66006ab
Adrian-Tudor Panescu <adrian@panescu.com>
2016-01-26 14:08:16 +0200

    rental images

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageToggle() {
+      this.set('isImageShowing', !this.get('isImageShowing'));
+    }
+  }
 });
